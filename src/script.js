f0e462c08aef40d3fb56b7dd8013679ebe8d25d9
Samuel Dudley <sbarkerdudley@gmail.com>
2022-02-04 21:55:34 -0800

    Basic sphere rendered in particles material

--- src/script.js
+++ src/script.js
@@ -1,95 +1,129 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'lil-gui'
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'lil-gui';
 
 /**
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI();
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const $canvas = document.querySelector('canvas');
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Textures
  */
-const textureLoader = new THREE.TextureLoader()
+const textureLoader = new THREE.TextureLoader();
+
+const geom = [
+    1,
+    80,
+    80,
+]
+// const geom = {
+//     radius: 50,
+//     widthSegments: 10,
+//     heightSegments: 10,
+// }
+
+
+const geometry = new THREE.SphereBufferGeometry(...geom)
+
+const particlesMaterial = new THREE.PointsMaterial();
+
+particlesMaterial.size = 0.005;
+particlesMaterial.sizeAttenuation = true;
+particlesMaterial.color = new THREE.Color(0xCF9060);
 
 /**
  * Test cube
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+const particles = new THREE.Points(geometry, particlesMaterial);
+scene.add(particles);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  w: window.innerWidth,
+  h: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.w = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.w / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.w, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
+const cam = {
+  fov: 75,
+  ratio: sizes.w / sizes.h,
+  near: .1,
+  far: 100,
+  z: 3,
+};
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+    cam.fov,
+    cam.ratio,
+    cam.near,
+    cam.far
+);
+camera.position.z = cam.z;
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, $canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: $canvas
+});
+
+renderer.setSize(sizes.w, sizes.h);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.antialias = true;
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const animate = () => {
+  const elapsedTime = clock.getElapsedTime();
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update();
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera);
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Call animate again on the next frame
+  window.requestAnimationFrame(animate);
+};
+
+animate();
+
+
+function vertices() {
+  return Array(500).fill().map(() => {})
 }
-
-tick()
